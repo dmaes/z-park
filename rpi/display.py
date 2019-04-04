be9fd75bde5436f75667be9cba4c98718b2ad40d
Dieter Maes <dieter.maes@dmaes.be>
2019-04-04 09:36:49 +0200

    revert to pin 2

--- rpi/display.py
+++ rpi/display.py
@@ -5,7 +5,7 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("https://z-park.eu-gb.mybluemix.net/parking/api/empty_spots")
 
-btn = Button(25)
+btn = Button(2)
 while True:
     if btn.is_pressed:
         driver.refresh()
