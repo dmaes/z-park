64e6407c01ba6f42ce49bb90739194dc73af1549
Dieter Maes <dieter.maes@dmaes.be>
2019-04-04 09:18:38 +0200

    change gpio-pin

--- rpi/display.py
+++ rpi/display.py
@@ -5,7 +5,7 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("https://z-park.eu-gb.mybluemix.net/parking/api/empty_spots")
 
-btn = Button(2)
+btn = Button(25)
 while True:
     if btn.is_pressed:
         driver.refresh()
